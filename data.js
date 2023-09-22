9df33c41e37423d7c5020b0961ce1ea35ea7fadf
unknown <kaushikgodhani2218@gmail.com>
2023-09-22 16:30:12 +0530

    backend api

---
+++ data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        channel:"jahkdsj", description:"djfhb", song:"hsdbjhs sdjshkdj", likes:100, messages:54, shares:10,
+    },
+    {
+        channel:"jahkdsj", description:"djfhb", song:"hsdbjhs sdjshkdj", likes:101, messages:55, shares:11,
+    }
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,62 @@
+import express  from "express";
+import mongoose from "mongoose";
+
+import data from "./data.js";
+import Videos from "./dbModel.js";
+
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+// middleware
+app.use(express.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader('Access-Control-Allow-Headers', '*'),
+    next()
+});
+
+// db config
+const connection_url = "mongodb+srv://rockknight54321:Rock%4054321@cluster0.yvidpg2.mongodb.net/tiktok" 
+
+mongoose.connect(connection_url, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+});
+
+// api end-point
+app.get('/', (req, res) => {
+    res.status(200).send("hello world");
+});
+
+app.get('/v1/posts/', (req, res) => {
+    res.status(200).send(data);
+});
+
+app.get('/v2/posts', (req, res)=>{
+    Videos.find().then((err, data)=>{
+        if(err){
+            res.status(200).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/v2/posts/', (req, res) => {
+    const dbVideos = req.body;
+
+    Videos.create( dbVideos).then((err, data)=>{
+        if(err){
+            res.status(201).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+// listener
+app.listen(port, () => {console.log(`listing on ${port}`)});
